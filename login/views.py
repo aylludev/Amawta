3d1e651762337b0e994c737103761b72b9e52271
aylludev <cafo1995@hotmail.com>
2025-01-24 11:13:45 -0500

    logout

--- login/views.py
+++ login/views.py
@@ -1,28 +1,14 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import FormView, LoginView, LogoutView
+from django.contrib.auth.views import FormView
 from django.http import HttpResponseRedirect
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 
-class LoginFormView(LoginView):
-    template_name = 'login.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect('erp:category_list')
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Iniciar sesion'
-        return context
-
-class LoginFormView2(FormView):
+class LoginFormView(FormView):
     form_class = AuthenticationForm
     template_name = 'login.html'
-    success_url = reverse_lazy('erp:category_list')
+    success_url = reverse_lazy('tag_create')
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -43,4 +29,4 @@ class LogoutRedirectView(RedirectView):
 
     def dispatch(self, request, *args, **kwargs):
         logout(request)
-# Create your views here.
+        return super().dispatch(request, *args, **kwargs)
--- yachay/urls.py
+++ yachay/urls.py
@@ -3,7 +3,7 @@ from yachay.views.tags.views import *
 
 urlpatterns = [
     path('tags/list/', TagListView.as_view(), name='category_list'),
-    path('tags/add/', TagCreateView.as_view(), name='category_create'),
+    path('tags/add/', TagCreateView.as_view(), name='tag_create'),
     path('tags/update/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
     path('tags/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
 ]
